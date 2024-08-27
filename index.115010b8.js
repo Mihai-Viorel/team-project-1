9761698a2844fd3c6879ea9a515cd37d279160d6
Mihai-Viorel <mike.vios.125@gmail.com>
2024-08-27 17:53:21 +0000

    Deploying to gh-pages from @ Mihai-Viorel/team-project-1@318c58fd379f49d84f09193ab27cb7227b892ce0 🚀

---
+++ index.115010b8.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".mySwiper",{spaceBetween:10,keyboard:{enabled:!0},pagination:{el:".swiper-pagination",type:"custom",renderCustom:function(e,n,a){return"0"+n+' <span class="pagination-separater"></span> 0'+a}},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.115010b8.js.map
