9911d876cbebc1f8666bbe90b996d5e4e2ff2ce2
Diana Martina S <64376820+dianamartina@users.noreply.github.com>
2024-08-27 20:20:05 +0300

    hero section

---
+++ src/script.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.mySwiper', {
+  spaceBetween: 10,
+  keyboard: {
+    enabled: true,
+  },
+  // effect: 'slide',
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'custom',
+    renderCustom: function (swiper, current, total) {
+      return (
+        '0' +
+        current +
+        ' <span class="pagination-separater"></span> ' +
+        '0' +
+        total
+      );
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
