ad596143da93a5ecb0a60dcc6dbbd68e15242972
Mihai-Viorel <mike.vios.125@gmail.com>
2024-09-03 06:40:35 +0000

    Deploying to gh-pages from @ Mihai-Viorel/team-project-1@6ffab290667781ca17583e5626cf660c05cdef26 🚀

---
+++ index.86c2886b.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".mySwiper",{spaceBetween:10,keyboard:{enabled:!0},pagination:{el:".swiper-pagination",type:"custom",renderCustom:function(e,n,t){return"0"+n+' <span class="pagination-separater"></span> 0'+t}},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});console.log("test");
+//# sourceMappingURL=index.86c2886b.js.map
---
+++ index.d36c751b.js
@@ -0,0 +1,2 @@
+var button=document.querySelector(".main_catalog_buy-now_button"),backdrop=document.querySelector(".modal-main_container");function toggleModal(){backdrop.classList.toggle("is-open")}button.addEventListener("click",toggleModal),backdrop.addEventListener("click",(function(o){o.target.closest(".btn-close-modal")&&toggleModal()}));
+//# sourceMappingURL=index.d36c751b.js.map
